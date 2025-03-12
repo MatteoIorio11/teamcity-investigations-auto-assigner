5e4f7d94512c6afb8c5f865cace67ee019004b3f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-03-12 17:14:46 +0100

    chore: remove old dependencies. Throw BuildProblemProcessorNotFoundException if the processor is not found

--- src/main/java/jetbrains/buildServer/investigationsAutoAssigner/utils/ProblemTextExtractor.java
+++ src/main/java/jetbrains/buildServer/investigationsAutoAssigner/utils/ProblemTextExtractor.java
@@ -2,21 +2,13 @@
 
 package jetbrains.buildServer.investigationsAutoAssigner.utils;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import jetbrains.buildServer.BuildProblemTypes;
-import jetbrains.buildServer.investigationsAutoAssigner.common.Constants;
-import jetbrains.buildServer.investigationsAutoAssigner.utils.errors.BuildProblemProcessor;
 import jetbrains.buildServer.investigationsAutoAssigner.utils.errors.BuildProblemProcessorFactory;
+import jetbrains.buildServer.investigationsAutoAssigner.utils.errors.BuildProblemProcessorNotFoundException;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.STest;
 import jetbrains.buildServer.serverSide.STestRun;
-import jetbrains.buildServer.serverSide.TeamCityProperties;
-import jetbrains.buildServer.serverSide.buildLog.LogMessage;
-import jetbrains.buildServer.serverSide.problems.BuildLogCompileErrorCollector;
 import jetbrains.buildServer.serverSide.problems.BuildProblem;
 import jetbrains.buildServer.tests.TestName;
-import jetbrains.buildServer.util.ItemProcessor;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -28,7 +20,7 @@ public class ProblemTextExtractor {
     return BuildProblemProcessorFactory.getProcessor(problem.getBuildProblemData().getType())
       .map(processor -> processor.process(problem, build, getCompileBlockIndex(problem)))
       .map(text -> text + " " + problem.getBuildProblemDescription())
-      .orElse("");
+      .orElseThrow(() -> new BuildProblemProcessorNotFoundException("Processor not found for problem type: " + problem.getBuildProblemData().getType()));
   }
 
   @Nullable
